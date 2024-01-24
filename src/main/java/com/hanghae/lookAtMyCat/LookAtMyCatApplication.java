ba2b394c0c0931257732c18fe56bb688a2703af7
soyoung <dbslzhs125@naver.com>
2024-01-25 03:37:47 +0900

    feat: first commit - SpringBoot 프로젝트 빌드

---
+++ src/main/java/com/hanghae/lookAtMyCat/LookAtMyCatApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.lookAtMyCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookAtMyCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookAtMyCatApplication.class, args);
+	}
+
+}
